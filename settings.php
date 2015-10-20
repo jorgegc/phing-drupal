64baebd36e78b93d3ed15ef42fb9912f6fef8b85
Jorge Castro <jorgegc@gmail.com>
2015-10-20 20:54:04 +0800

    Create settings.php file if it does not already exist.

---
+++ settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Load local development override configuration, if available.
+ *
+ * Use settings.local.php to override variables on secondary (staging,
+ * development, etc) installations of this site. Typically used to disable
+ * caching, JavaScript/CSS compression, re-routing of outgoing emails, and
+ * other things that should not happen on development and testing sites.
+ *
+ * Keep this code block at the end of this file to take full effect.
+ */
+if (file_exists(__DIR__ . '/settings.local.php')) {
+  include __DIR__ . '/settings.local.php';
+}
